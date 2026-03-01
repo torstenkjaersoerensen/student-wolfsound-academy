c4427dd4283ee52952b6ca1ed0c7fa4cf440af46
Torsten Kjær Sørensen <torstenkjaersoerensen@gmail.com>
2026-03-01 09:47:18 +0100

    Add GoogleTest framework and initial unit test setup

---
+++ dsp-pro/test/source/bandpass_plugin/AudioProcessorTest.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Torsten Kjær Sørensen on 28/02/2026.
+//
+
+#include <gtest/gtest.h>
+#include <PluginProcessor.h>
+
+namespace audio_plugin_test {
+    TEST(AudioPluginAudioProcessor, Foo) {
+        AudioPluginAudioProcessor processor;
+        (void)processor;
+        ASSERT_FALSE(true);
+    }
+}
